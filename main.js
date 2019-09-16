c5a0307fb89033d872e5a7486ab35b844bf1ae8c
edo9k <edox86@gmail.com>
2019-09-16 13:48:42 -0300

    this is a test commit
    blablabla

--- main.js
+++ main.js
@@ -10,4 +10,6 @@ function sum(a, b) {
 // cast permissive boolean checker sortof
 const isBool = x => typeof x === 'boolean' || x === 'true' || x === 'false' || x === 0 || x === 1
 
-module.exports = { sum, isBool }
+const validateId = id => id && validator.isUUID(id)
+
+module.exports = { sum, isBool, validateId }
--- main.test.js
+++ main.test.js
@@ -1,19 +1,37 @@
 /** @format */
 
-const { sum, isBool } = require('./main')
+const { sum, isBool, validateId } = require('./main')
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
+describe('tests sum function', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3)
+  })
 })
 
-test('check that variable is boolean', () => {
-  expect(isBool(true)).toBe(true)
-  expect(isBool('')).toBe(false)
-  expect(isBool()).toBe(false)
-  expect(isBool(null)).toBe(false)
-  expect(isBool(undefined)).toBe(false)
-  expect(isBool(1)).toBe(true)
-  expect(isBool(0)).toBe(true)
-  expect(isBool('true')).toBe(true)
-  expect(isBool('false')).toBe(true)
+describe('tests validateId function', () => {
+  it('check that variable is boolean', () => {
+    expect(isBool(true)).toBe(true)
+    expect(isBool('')).toBe(false)
+    expect(isBool()).toBe(false)
+    expect(isBool(null)).toBe(false)
+    expect(isBool(undefined)).toBe(false)
+    expect(isBool(1)).toBe(true)
+    expect(isBool(0)).toBe(true)
+    expect(isBool('true')).toBe(true)
+    expect(isBool('false')).toBe(true)
+  })
+})
+
+describe('tests validateId function', () => {
+  it('check that variable is valid UUID', () => {
+    //expect(validateId(true)).toBe(false) // must be string, error!
+    expect(validateId('')).toBe(false)
+    expect(validateId()).toBe(false)
+    expect(validateId(null)).toBe(false)
+    expect(validateId(undefined)).toBe(false)
+    expect(validateId(1)).toBe(false)
+    expect(validateId(0)).toBe(false)
+    expect(validateId()).toBe(false)
+    expect(validateId('123e4567-e89b-12d3-a456-426655440000')).toBe(true)
+  })
 })
